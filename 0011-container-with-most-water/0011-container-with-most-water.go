adfd511a24f4919b6b131565c6be50091498722d
domicmeia <71815610+domicmeia@users.noreply.github.com>
2025-01-12 19:36:19 +0900

    Time: 0 ms (100%), Space: 9.9 MB (34.28%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.go
@@ -0,0 +1,20 @@
+func maxArea(height []int) int {
+    max, start, end := 0, 0, len(height) - 1
+    for start < end {
+        x := end - start
+        y := 0
+        if height[start] < height[end] {
+            y = height[start]
+            start ++
+        } else {
+            y = height[end]
+            end --
+        }
+
+        tmp := x * y
+        if tmp > max {
+            max = tmp
+        }
+    }
+    return max
+}
